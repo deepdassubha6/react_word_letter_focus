0bd2d98b7bc4c143525d1735d1e7576e21a4fb4e
Subhadeep Das <subhadeep.das@learningmate.com>
2018-07-22 19:04:57 +0530

    tools logic implement

--- src/js/components/Main.js
+++ src/js/components/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import Header from "./Main/Sentence";
 import MainBody from "./Main/Word";
+import Tool from "./Main/Tool";
 
 import Styles from "./main.css";
 
@@ -59,28 +60,24 @@ export default class Main extends React.Component {
                     <h2 class={Styles.direction} dangerouslySetInnerHTML={{ __html: this.direction }}></h2>
                     <div class="content">
                         <div class={Styles.slideData}>
-                            {/* <div class="slide-section" role="application">
-                                <div class="answer-section">
+                            <div class="slide-section" role="application">
+                                <div class={Styles.answerSection}>
                                     <div class="col-xs-15" ng-repeat="qstn in qstnSets">
-                                        <div droppable id="droppableBin{{$index}}" class="answer-box droppable" ng-class="{'right':$last}" tabindex="0" aria-label="droppable {{qstn.trimedTitle}}">
+                                        <div droppable id="droppableBin{{$index}}" class="answer-box droppable" ng-class="{'right':$last}" tabIndex="0" aria-label="droppable {{qstn.trimedTitle}}">
                                             <div class="content-title" ng-bind-html="qstn.title" ></div>
                                         </div>
                                     </div>
                                     <div class="clearfix"></div>
                                 </div>
                                 <div class="clearfix"></div>
-                                <div class="droppable wordContainer input-texts col-md-12" id="main-drop-container" aria-label="droppable base region">
-                                    <span class="droppable-initial" ng-repeat="option in options" id="droppable{{$index}}" rel={{$index}}>
-                                        <span class="draggable"  id="draggable{{$index}}" ng-bind-html="option" tabindex="0" rel={{$index}} aria-label="draggable {{option}}"></span>
-                                    </span>
-                                </div>
-                            </div> */}
+                                <Tool />
+                            </div>
                             <div class="clearfix"></div>
                         </div>
 
                         <section class={Styles.buttonSection}>
-                            <button class={`${Styles.resetButton} ${Styles.button}`} style={{ backgroundImage: `url(./images/icons/reset_btn.png)` }} onClick="reset()" aria-label="Reset"></button>
-                            <button class={`${Styles.okButton} ${Styles.button}`} style={{ backgroundImage: `url(./images/icons/ok_btn.png)` }} onClick="validate()" aria-label="Ok" aria-live="assertive"></button>
+                            <button class={`${Styles.resetButton} ${Styles.button}`} style={{ backgroundImage: `url(./images/icons/reset_btn.png)` }} onClick={this.reset.bind(this)} aria-label="Reset"></button>
+                            <button class={`${Styles.okButton} ${Styles.button}`} style={{ backgroundImage: `url(./images/icons/ok_btn.png)` }} onClick={this.validate.bind(this)} aria-label="Ok" aria-live="assertive"></button>
                         </section>
                     </div>
                     <div class="clear"></div>
---
+++ src/js/components/Main/Tool.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import Styles from "./tool.css";
+import "./tool.css";
+
+export default class Tpol extends React.Component {
+    constructor() {
+        super();
+        this.toolItems = '';
+        this.toolList = ['word Highlight', 'word Underline', 'letter Identification', 'word Divide', 'erase'];
+        this.populateTools(this.toolList);
+    }
+    setToolName(toolName) {
+       window.toolName = toolName;
+    }
+    populateTools(toolList) {
+        this.toolItems = toolList.map((tool) => {
+            let itemName = tool.replace(/\s/g, '');
+            return (
+                <span key={tool} class = "each-tool-wrapper" onClick={this.setToolName.bind(this, `${tool}` )} style = { {display: 'inline-block' }}>
+                    <span class = { Styles[`${itemName}`]}  style={{ backgroundImage: `url(./images/icons/${itemName}.png)` }}></span>
+                    <br/>
+                    <span class={Styles.toolInitial} aria-lable={tool}>{tool}</span>
+                </span>
+            )
+        });
+    }
+
+    render() {
+        return (
+            <div class={`${Styles.toolsWrapper} toolContainer col-md-12`} id="toolContainer">
+                {this.toolItems}
+            </div>
+        );
+    }
+}
